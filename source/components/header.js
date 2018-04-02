5936345446b432796cab56e0b5a8175ab3fd312c
Antonio Junior <junyor@hotmail.co.uk>
2018-04-02 02:50:46 -0300

    Creating header component.

---
+++ source/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="as-header">
+    <div className="as-header-container">
+      <h1 className="as-header-title">Do it now</h1>
+      <p className="as-user">Usuário 1</p>
+    </div>
+  </header>
+);
+
+export default Header;
