9b0651d424c52ae22738e89188080469d163a920
Antonio Junior <junyor@hotmail.co.uk>
2018-04-02 02:43:25 -0300

    Creating static list file.

---
+++ source/static/index.js
@@ -0,0 +1,50 @@
+export const lists = [
+  {
+    name: 'Lista 1',
+    tasks: [
+      {
+        name: 'Tarefa 1',
+        checked: false,
+        subtasks: [
+          {
+            name: 'Subtarefa 1',
+            checked: false
+          },
+          {
+            name: 'Subtarefa 2',
+            checked: false
+          },
+          {
+            name: 'Subtarefa 3',
+            checked: false
+          },
+        ]
+      },
+      {
+        name: 'Tarefa 2',
+        checked: false,
+        subtasks: []
+      }
+    ]
+  },
+  {
+    name: 'Lista 2',
+    tasks: [
+      {
+        name: 'Tarefa 1',
+        checked: false,
+        subtasks: []
+      }
+    ]
+  },
+  {
+    name: 'Lista 3',
+    tasks: [
+      {
+        name: 'Tarefa 1',
+        checked: false,
+        subtasks: []
+      }
+    ]
+  }
+]
