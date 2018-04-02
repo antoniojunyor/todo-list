d2b471668fe31fa59cee8c4ec4dbac07aa4fd796
Antonio Junior <junyor@hotmail.co.uk>
2018-04-02 02:35:43 -0300

    Creating base project structure (app with routes).

---
+++ source/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Routes from './routes';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="as-main">
+        <Routes />
+      </main>
+    );
+  }
+}
---
+++ source/routes/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/home';
+import Login from '../pages/login';
+import CreateAccount from '../pages/create-account';
+import MyLists from '../pages/my-lists';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/login" component={Login}/>
+    <Route exact path="/create-account" component={CreateAccount}/>
+    <Route exact path="/my-lists" component={MyLists}/>
+  </Switch>
+);
+
+export default Routes;
