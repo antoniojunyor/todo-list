0fd5507052cfb091c3c601bd2c30228ec554e962
Antonio Junior <junyor@hotmail.co.uk>
2018-04-02 02:36:44 -0300

    Creating app style file.

--- source/app.js
+++ source/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Routes from './routes';
 
+import './assets/stylesheets/app.sass';
+
 export default class App extends Component {
   render() {
     return (
