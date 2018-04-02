281d928103fb3d0096705ca8964d1be8a477fb8b
Antonio Junior <junyor@hotmail.co.uk>
2018-04-02 03:51:56 -0300

    Adjustment on subtask component.

--- source/components/subtask.js
+++ source/components/subtask.js
@@ -5,14 +5,12 @@ const Subtask = ({
   subtask,
   onClickDeleteSubtask,
   onChangeSubtask
-}) => {
-  return (
-    <li className="as-subtask-item">
-      <input type="checkbox" checked={subtask.checked} onChange={() => onChangeSubtask(subtaskIndex)}/>
-      <span>{subtask.name}</span>
-      <button className="as-button-delete-subtask" onClick={() => onClickDeleteSubtask(subtaskIndex)}>excluir subtarefa</button>
-    </li>
-  );
-}
+}) => (
+  <li className="as-subtask-item">
+    <input type="checkbox" checked={subtask.checked} onChange={() => onChangeSubtask(subtaskIndex)}/>
+    <span>{subtask.name}</span>
+    <button className="as-button-delete-subtask" onClick={() => onClickDeleteSubtask(subtaskIndex)}>excluir subtarefa</button>
+  </li>
+);
 
 export default Subtask;
