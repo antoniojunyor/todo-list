8cb3c917cb7805513fcda51910c9675ec6403a15
Antonio Junior <junyor@hotmail.co.uk>
2018-04-03 00:41:57 -0300

    Adding uuid on static list array.

--- source/static/index.js
+++ source/static/index.js
@@ -1,26 +1,34 @@
+import uuidv1 from 'uuid/v1';
+
 export const lists = [
   {
+    id: uuidv1(),
     name: 'Lista 1',
     tasks: [
       {
+        id: uuidv1(),
         name: 'Tarefa 1',
         checked: false,
         subtasks: [
           {
+            id: uuidv1(),
             name: 'Subtarefa 1',
             checked: false
           },
           {
+            id: uuidv1(),
             name: 'Subtarefa 2',
             checked: false
           },
           {
+            id: uuidv1(),
             name: 'Subtarefa 3',
             checked: false
           },
         ]
       },
       {
+        id: uuidv1(),
         name: 'Tarefa 2',
         checked: false,
         subtasks: []
@@ -28,9 +36,11 @@ export const lists = [
     ]
   },
   {
+    id: uuidv1(),
     name: 'Lista 2',
     tasks: [
       {
+        id: uuidv1(),
         name: 'Tarefa 1',
         checked: false,
         subtasks: []
@@ -38,9 +48,11 @@ export const lists = [
     ]
   },
   {
+    id: uuidv1(),
     name: 'Lista 3',
     tasks: [
       {
+        id: uuidv1(),
         name: 'Tarefa 1',
         checked: false,
         subtasks: []
