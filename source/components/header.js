4971b386ce414ee76549ac62a43b15866605925c
Antonio Junior <junyor@hotmail.co.uk>
2018-04-06 07:20:52 -0300

    Adding link on header to my lists.

--- source/components/header.js
+++ source/components/header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header className="as-header">
     <div className="as-header-container">
-      <h1 className="as-header-title">Do it now</h1>
+      <NavLink to="/my-lists" activeClassName="as-active">
+        <h1 className="as-header-title">Do it now</h1>
+      </NavLink>
       <p className="as-user">Usuário 1</p>
     </div>
   </header>
