2c6a09a8cf1f1ef37325c32a6caa5ccba9df3284
Antonio Junior <junyor@hotmail.co.uk>
2018-04-06 07:16:57 -0300

    Improvements on lists components and adding new methods.

--- source/components/lists.js
+++ source/components/lists.js
@@ -5,7 +5,22 @@ export default class Lists extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nextTaskValue: ''
+      nextTaskValue: '',
+      ListName: this.props.list.name
+    }
+
+    this.onChangeListName = this.onChangeListName.bind(this);
+    this.toggleListInput = this.toggleListInput.bind(this);
+  }
+
+  onChangeListName(event) {
+    this.setState({ listName: event.target.value }, () => this.props.onEditListName(this.state.listName));
+  }
+
+  toggleListInput(event) {
+    if (event) event.preventDefault();
+    if (this.state.ListName) {
+      document.querySelectorAll('.as-form-list-name input')[this.props.listIndex].classList.toggle('as-active-field');
     }
   }
 
@@ -20,8 +35,12 @@ export default class Lists extends Component {
       onAddTask,
       onChangeFormAddTask,
       listIndex,
-      onClickAddSubtask
+      onClickAddSubtask,
+      showInputTask,
+      showInputSubtask
     } = this.props;
+
+    let addTask;
 
     const listTasks = list.tasks.map((task, index) => {
       return (
@@ -34,28 +53,38 @@ export default class Lists extends Component {
           onChangeTask={taskIndex => onChangeTask(taskIndex, listIndex)}
           onChangeSubtask={(subtaskIndex, taskIndex) => onChangeSubtask(subtaskIndex, taskIndex, listIndex)}
           onClickAddSubtask={(nextSubtaskValue, taskIndex) => onClickAddSubtask(nextSubtaskValue, taskIndex, listIndex)}
+          showInputSubtask={showInputSubtask}
         />
       );
     });
 
+    if (showInputTask !== false) {
+      addTask = (
+        <li className="as-add-task">
+          <form className="as-form-add-task" onSubmit={() => onAddTask(this.state.nextTaskValue, listIndex)} onChange={event => this.setState({ nextTaskValue: event.target.value })}>
+            <input type="text" placeholder="Adicionar Tarefa"/>
+            <button type="submit" className="as-button-add-task">Adicionar Tarefa</button>
+          </form>
+        </li>
+      );
+    }
+
     return (
       <li className="as-lists-item">
         <div className="as-list-header">
-          <input type="text" defaultValue={list.name}/>
+          <form className="as-form-list-name" onSubmit={this.toggleListInput}>
+            <input type="text" value={this.state.ListName} onChange={this.onChangeListName}/>
+            <button type="submit">Alterar nome da lista</button>
+          </form>
+
           <div className="as-list-actions">
-            <button type="button" className="as-button-edit-list">Editar Lista</button>
+            <button type="button" className="as-button-edit-list" onClick={this.toggleListInput}>Editar Lista</button>
             <button type="button" className="as-button-delete-list" onClick={() => onClickDeleteList(listIndex)}>Excluir Lista</button>
           </div>
         </div>
 
         <ul className="as-task-list">
-          <li className="as-add-task">
-            <form className="as-form-add-task" onSubmit={() => onAddTask(this.state.nextTaskValue, listIndex)} onChange={event => this.setState({ nextTaskValue: event.target.value })}>
-              <input type="text" placeholder="Adicionar Tarefa"/>
-              <button type="submit" className="as-button-add-task">Adicionar Tarefa</button>
-            </form>
-          </li>
-
+          {addTask}
           {listTasks}
         </ul>
       </li>
