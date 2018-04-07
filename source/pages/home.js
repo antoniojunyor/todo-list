679e5fe5abd39a5d370185116e52c8ca117ec5a9
Antonio Junior <junyor@hotmail.co.uk>
2018-04-07 19:36:50 -0300

    Transforming home on function component.

--- source/pages/home.js
+++ source/pages/home.js
@@ -3,17 +3,13 @@ import { Link } from 'react-router-dom';
 
 import imgResponsive from '../assets/images/responsive.png';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="as-home-page">
-        <div className="as-wrap-content">
-          <img src={imgResponsive} alt="Devices Responsivos"/>
-          <h1>Do it now</h1>
-          <Link to="/create-account" className="as-button-orange"><span>Criar Conta</span></Link>
-          <Link to="/login" className="as-button-green"><span>Entrar</span></Link>
-        </div>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="as-home-page">
+    <div className="as-wrap-content">
+      <img src={imgResponsive} alt="Devices Responsivos"/>
+      <h1>Do it now</h1>
+      <Link to="/create-account" className="as-button-orange"><span>Criar Conta</span></Link>
+      <Link to="/login" className="as-button-green"><span>Entrar</span></Link>
+    </div>
+  </div>
+);
